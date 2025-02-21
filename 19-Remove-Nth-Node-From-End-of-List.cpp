d6202bf5c2cfbe8bb0a9e0a22ded4922c5a087ac
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-02-22 02:58:08 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 59.57%] - LeetPush

--- 19-Remove-Nth-Node-From-End-of-List.cpp
+++ 19-Remove-Nth-Node-From-End-of-List.cpp
@@ -11,31 +11,75 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // ListNode* temp = head;
+        // int count  = 1;
+        // if(head == nullptr){
+        //     return head;
+        // }
+        // if(head -> next == nullptr){
+        //     head = nullptr;
+        //     return head;
+        // }
+        // while(temp -> next != nullptr){
+        //     temp = temp -> next;
+        //     count  = count + 1;
+        // }
+        // temp = head;
+        // int c = 1;
+        // if(n == count){
+        //     head = head -> next;
+        // }
+        // else{
+        //     while(c < count - n){
+        //         temp = temp -> next;
+        //         c++;
+        //     }
+        // }
+        // temp -> next = temp-> next -> next;
+        // return head;
+
+
+
+
+
+
+
+
+
+
+
+
+
         ListNode* temp = head;
-        int count  = 1;
-        if(head == nullptr){
-            return head;
-        }
+        int tcount = 1;
         if(head -> next == nullptr){
-            head = nullptr;
-            return head;
+            return nullptr;
         }
         while(temp -> next != nullptr){
-            temp = temp -> next;
-            count  = count + 1;
+            temp = temp -> next; 
+            tcount++;
         }
-        temp = head;
-        int c = 1;
-        if(n == count){
+        if(tcount -n == 0){
             head = head -> next;
         }
         else{
-            while(c < count - n){
-                temp = temp -> next;
-                c++;
+            int count = 1;
+            ListNode* temp2 = head;
+            while(count < tcount - n+1){
+                if(count == tcount -n){
+                    temp2 -> next = temp2 -> next -> next;
+                }
+                else{
+                    temp2 = temp2 -> next;
+                }
+                count++;
             }
         }
-        temp -> next = temp-> next -> next;
         return head;
+
+
+
+
+
     }
 };
