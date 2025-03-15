5c7e8c4b5a65877a2f651f77f55a34f23b853a7e
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-03-15 21:27:37 +0530

    [1] [Time Beats: 6.25%] [Memory Beats: 61.10%] - LeetPush

---
+++ Two-Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    //     int n = nums.size();
+
+    //     for(int i = 0; i <= n-1; i ++){
+    //         for(int j = i + 1; j <= n-1; j ++){
+    //             if(nums[i] + nums[j] == target){
+    //             return {i,j};
+    //             }
+    //         }
+    //     }
+    //     return {};
+    // }
+
+    int n  = nums.size();
+    vector<int> ans;
+
+        for(int i = 0; i < n;i++){
+            for(int j = i+1;j < n; j++){
+                if(nums[i] + nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
